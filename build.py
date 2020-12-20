7538c382cefcf64a1f7bab28c72b39dc90bbb715
代小呆 <diauweb@live.com>
2020-12-20 07:44:54 +0000

    feat: sync sidebar

--- build.py
+++ build.py
@@ -69,6 +69,13 @@ class Build:
         
         self.__run("node", ".", cwd=f"./migrator")
     
+    def __gen_nav(self):
+        with open("./doc/mkdocs.yml", "r") as f:
+            mkdoc_cfg = yaml.load(f, yaml.BaseLoader)
+            os.makedirs("./cauldron/src/gatsby-theme-oi-wiki/", exist_ok=True)
+            with open("./cauldron/src/gatsby-theme-oi-wiki/sidebar.yaml", "w") as fs:
+                yaml.dump(mkdoc_cfg["nav"], fs, allow_unicode=True)
+
     def prepare_cauldron(self):
         print("build: prepare cauldron")
         if os.path.exists("./cauldron"):
@@ -81,6 +88,7 @@ class Build:
         shutil.copy("./test.md", "./cauldron/docs/test.md")
         shutil.copytree("./doc/.git", "./cauldron/.git")
 
+        self.__gen_nav()
         self.__run("yarn", "install", cwd="./cauldron")
 
     def generate(self):
