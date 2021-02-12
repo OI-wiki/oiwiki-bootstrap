eb9c6c2c940891b1a00bb920e863318aed5ef9e5
代小呆 <diauweb@live.com>
2021-02-12 10:51:53 +0000

    feat: shadow files in cauldron
    fix #1

--- build.py
+++ build.py
@@ -107,11 +107,22 @@ class Build:
         shutil.copytree("./jamstack/example", "./cauldron", 
             ignore=shutil.ignore_patterns("docs", "package.json"))
         shutil.copytree("./migrator/out/docs", "./cauldron/docs")
-        shutil.copy("./build_package.json", "./cauldron/package.json")
-        shutil.copy("./build_gatsby-node.js", "./cauldron/gatsby-node.js")
-        shutil.copy("./build.env.production", "./cauldron/.env.production")
-        shutil.copy("./test.md", "./cauldron/docs/test.md")
         shutil.copytree("./doc/.git", "./cauldron/.git")
+        
+        root_src_dir = "./shadow"
+        root_dst_dir = "./cauldron"
+        for src_dir, dirs, files in os.walk(root_src_dir):
+            dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    if os.path.samefile(src_file, dst_file):
+                        continue
+                    os.remove(dst_file)
+                shutil.copy(src_file, dst_dir)
 
         self.__gen_nav()
         self.__run("yarn", "install", cwd="./cauldron")
