2e66a4c8370fc3d10643f71381a8e5dc1e51e8a0
代小呆 <diauweb@live.com>
2021-07-26 16:37:08 +0800

    fix: update site metadata

--- shadow/gatsby-config.js
+++ shadow/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: 'OI Wiki',
-    description: 'Guide for OI',
+    description: 'OI Wiki 是一个编程竞赛知识整合站点，提供有趣又实用的编程竞赛知识以及其他有帮助的内容，帮助广大编程竞赛爱好者更快更深入地学习编程竞赛',
     author: 'OI Wiki Team',
-    siteUrl: 'https://ng.oi-wiki.org',
+    siteUrl: 'https://next.oi-wiki.org',
   },
   plugins: [
     {
